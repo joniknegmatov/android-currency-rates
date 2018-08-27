72d92a2bba3b4efa36cd2a67aee028ef1e236f90
starlabs <jonibek.negmatov@mail.ru>
2018-08-27 15:36:33 +0500

    - Added test for currency helper

--- app/src/test/java/io/jonibek/currency/ExampleUnitTest.kt
+++ app/src/test/java/io/jonibek/currency/ExampleUnitTest.kt
@@ -1,6 +1,7 @@
 package io.jonibek.currency
 
 import io.jonibek.currency.data.local.CurrencyContainer
+import io.jonibek.currency.util.CurrencyHelper
 import org.junit.Test
 
 /**
@@ -13,14 +14,20 @@ class ExampleUnitTest {
     @Test
     fun testCurrencyContainer() {
 
-        val currencyRateMap = mapOf("USD" to  1.411f,
+        val currencyRateMap = mapOf("USD" to 1.411f,
                 "EUR" to 1.12f,
                 "UZS" to 7800f)
 
         val currencyContainer = CurrencyContainer("GBP", currencyRateMap)
 
         assert(currencyContainer.getCurrenciesAmount() == 4)
-        assert(currencyContainer.getCurrencyNameAndRate(0).first == "USD")
-        assert(currencyContainer.getCurrencyNameAndRate(0).second == 1.411f)
+        assert(currencyContainer.getCurrencyNameAndRate(1).first == "USD")
+        assert(currencyContainer.getCurrencyNameAndRate(1).second == 1.411f)
+    }
+
+    @Test
+    fun testCurrencyHelper() {
+       assert(CurrencyHelper.getCurrencyInfo("USD").first == "US Dollar")
+       assert(CurrencyHelper.getCurrencyInfo("USD").second == "$")
     }
 }
